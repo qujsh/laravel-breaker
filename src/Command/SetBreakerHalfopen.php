345caf977e0119c6d55748310d3e91785ecea44e
qujsh <ningmingqaz@126.com>
2019-07-29 14:25:06 +0800

    熔断器 功能调整 做测试使用

--- src/Command/SetBreakerHalfopen.php
+++ src/Command/SetBreakerHalfopen.php
@@ -18,16 +18,25 @@ class SetBreakerHalfopen extends Command
         // 在命令行打印一行信息
         $this->info("开始...");
 
-        $circuit_key = 'circuit';
-        $circuit_halfopen = 'circuit_halfopen';
-        $attempts = config('breaker.default.attempts');
-
+        $config = config('breaker');
         while (true){
 
-            $time = \Cache::get($circuit_halfopen);
-            if ($time && $time<=time()){
-                \Cache::forever($circuit_key, 0-$attempts);          //设置 <0 为半开状态
-                \Cache::forget($circuit_halfopen);      //做删除，不然会一直执行下去
+            //因为 可以根据配置文件做多service 不同情景使用，所以 做循环使用
+            foreach ($config as $key => $value){
+                if (is_array($value)){
+
+                    $circuit_key = 'circuit_'.$key;
+                    $circuit_halfopen = 'circuit_halfopen_'.$key;
+                    $attempts = $value['attempts'];
+
+                    $time = \Cache::get($circuit_halfopen);
+                    if ($time && $time<=time()){
+                        \Cache::forever($circuit_key, 0-$attempts);          //设置 <0 为半开状态
+                        \Cache::forget($circuit_halfopen);      //做删除，不然会一直执行下去
+                    }
+                }else{
+                    break;
+                }
             }
 
             usleep(500*1000);           //休眠500毫秒
